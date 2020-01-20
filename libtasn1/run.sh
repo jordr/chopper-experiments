027eb7a5ab36084475a9f3d18371dd30603f7f40
Jordy Ruiz <jruiz@ic.ac.uk>
2020-01-20 12:04:37 +0000

    Only run benchmarks up to -20

--- libtasn1/run.sh
+++ libtasn1/run.sh
@@ -22,14 +22,14 @@ terminate_cse cse-split-searcher-10
 make KSLICE="-split-search -split-ratio=20" all-cse
 terminate_cse cse-split-searcher-20
 
-make KSLICE="-split-search -split-ratio=30" all-cse
-terminate_cse cse-split-searcher-30
+# make KSLICE="-split-search -split-ratio=30" all-cse
+# terminate_cse cse-split-searcher-30
 
-make KSLICE="-split-search -split-ratio=40" all-cse
-terminate_cse cse-split-searcher-40
+# make KSLICE="-split-search -split-ratio=40" all-cse
+# terminate_cse cse-split-searcher-40
 
-make KSLICE="-split-search -split-ratio=50" all-cse
-terminate_cse cse-split-searcher-50
+# make KSLICE="-split-search -split-ratio=50" all-cse
+# terminate_cse cse-split-searcher-50
 
 # Running CSE with CSE-optimized searcher
 make KSLICE="-recovery-search=random-path -split-ratio=10" all-cse
@@ -38,14 +38,14 @@ terminate_cse cse-recovery-searcher-rp-10
 make KSLICE="-recovery-search=random-path -split-ratio=20" all-cse
 terminate_cse cse-recovery-searcher-rp-20
 
-make KSLICE="-recovery-search=random-path -split-ratio=30" all-cse
-terminate_cse cse-recovery-searcher-rp-30
+# make KSLICE="-recovery-search=random-path -split-ratio=30" all-cse
+# terminate_cse cse-recovery-searcher-rp-30
 
-make KSLICE="-recovery-search=random-path -split-ratio=40" all-cse
-terminate_cse cse-recovery-searcher-rp-40
+# make KSLICE="-recovery-search=random-path -split-ratio=40" all-cse
+# terminate_cse cse-recovery-searcher-rp-40
 
-make KSLICE="-recovery-search=random-path -split-ratio=50" all-cse
-terminate_cse cse-recovery-searcher-rp-50
+# make KSLICE="-recovery-search=random-path -split-ratio=50" all-cse
+# terminate_cse cse-recovery-searcher-rp-50
 
 make KSLICE="-recovery-search=dfs -split-ratio=10" all-cse
 terminate_cse cse-recovery-searcher-dfs-10
@@ -53,14 +53,14 @@ terminate_cse cse-recovery-searcher-dfs-10
 make KSLICE="-recovery-search=dfs -split-ratio=20" all-cse
 terminate_cse cse-recovery-searcher-dfs-20
 
-make KSLICE="-recovery-search=dfs -split-ratio=30" all-cse
-terminate_cse cse-recovery-searcher-dfs-30
+# make KSLICE="-recovery-search=dfs -split-ratio=30" all-cse
+# terminate_cse cse-recovery-searcher-dfs-30
 
-make KSLICE="-recovery-search=dfs -split-ratio=40" all-cse
-terminate_cse cse-recovery-searcher-dfs-40
+# make KSLICE="-recovery-search=dfs -split-ratio=40" all-cse
+# terminate_cse cse-recovery-searcher-dfs-40
 
-make KSLICE="-recovery-search=dfs -split-ratio=50" all-cse
-terminate_cse cse-recovery-searcher-dfs-50
+# make KSLICE="-recovery-search=dfs -split-ratio=50" all-cse
+# terminate_cse cse-recovery-searcher-dfs-50
 
 cd ..
 }
