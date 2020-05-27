f3db30b856f7f1a0c45558a090c5158d40b3d051
Jordy Ruiz <jruiz@ic.ac.uk>
2020-05-27 18:06:53 +0200

    Added configure-chopit.sh

---
+++ libtasn1/configure-chopit.sh
@@ -0,0 +1,57 @@
+mkdir CVE-2012-1569/nurs/
+mkdir CVE-2012-1569/dfs/
+mkdir CVE-2012-1569/random/
+mkdir CVE-2014-3467/nurs-1/
+mkdir CVE-2014-3467/dfs-1/
+mkdir CVE-2014-3467/random-1/
+mkdir CVE-2014-3467/nurs-2/
+mkdir CVE-2014-3467/dfs-2/
+mkdir CVE-2014-3467/random-2/
+mkdir CVE-2014-3467/nurs-3/
+mkdir CVE-2014-3467/dfs-3/
+mkdir CVE-2014-3467/random-3/
+mkdir CVE-2015-2806/nurs/
+mkdir CVE-2015-2806/dfs/
+mkdir CVE-2015-2806/random/
+mkdir CVE-2015-3622/nurs/
+mkdir CVE-2015-3622/dfs/
+mkdir CVE-2015-3622/random/
+mkdir CVE-2015-3622/random/
+
+ln -s ../test.bc CVE-2012-1569/nurs/test.bc
+ln -s ../def.asn CVE-2012-1569/nurs/def.asn
+ln -s ../test.bc CVE-2012-1569/dfs/test.bc
+ln -s ../def.asn CVE-2012-1569/dfs/def.asn
+ln -s ../test.bc CVE-2012-1569/random/test.bc
+ln -s ../def.asn CVE-2012-1569/random/def.asn
+ln -s ../test.bc CVE-2014-3467/nurs-1/test.bc
+ln -s ../def.asn CVE-2014-3467/nurs-1/def.asn
+ln -s ../test.bc CVE-2014-3467/dfs-1/test.bc
+ln -s ../def.asn CVE-2014-3467/dfs-1/def.asn
+ln -s ../test.bc CVE-2014-3467/random-1/test.bc
+ln -s ../def.asn CVE-2014-3467/random-1/def.asn
+ln -s ../test.bc CVE-2014-3467/nurs-2/test.bc
+ln -s ../def.asn CVE-2014-3467/nurs-2/def.asn
+ln -s ../test.bc CVE-2014-3467/dfs-2/test.bc
+ln -s ../def.asn CVE-2014-3467/dfs-2/def.asn
+ln -s ../test.bc CVE-2014-3467/random-2/test.bc
+ln -s ../def.asn CVE-2014-3467/random-2/def.asn
+ln -s ../test.bc CVE-2014-3467/nurs-3/test.bc
+ln -s ../def.asn CVE-2014-3467/nurs-3/def.asn
+ln -s ../test.bc CVE-2014-3467/dfs-3/test.bc
+ln -s ../def.asn CVE-2014-3467/dfs-3/def.asn
+ln -s ../test.bc CVE-2014-3467/random-3/test.bc
+ln -s ../def.asn CVE-2014-3467/random-3/def.asn
+ln -s ../test.bc CVE-2015-2806/nurs/test.bc
+ln -s ../def.asn CVE-2015-2806/nurs/def.asn
+ln -s ../test.bc CVE-2015-2806/dfs/test.bc
+ln -s ../def.asn CVE-2015-2806/dfs/def.asn
+ln -s ../test.bc CVE-2015-2806/random/test.bc
+ln -s ../def.asn CVE-2015-2806/random/def.asn
+ln -s ../test.bc CVE-2015-3622/nurs/test.bc
+ln -s ../def.asn CVE-2015-3622/nurs/def.asn
+ln -s ../test.bc CVE-2015-3622/dfs/test.bc
+ln -s ../def.asn CVE-2015-3622/dfs/def.asn
+ln -s ../test.bc CVE-2015-3622/random/test.bc
+ln -s ../def.asn CVE-2015-3622/random/def.asn
+ln -s ../test.bc CVE-2015-3622/random/test.bc
