cde033a5b8832633a1f2770b718a03da6b51443f
Jordy Ruiz <jruiz@ic.ac.uk>
2020-09-17 01:07:55 +0100

    Improve run-example.sh

--- auto-libtasn1/run-example.sh
+++ auto-libtasn1/run-example.sh
@@ -1,1 +1,3 @@
-chopem.py libtasn1-git --new-revision=libtasn1_2_11 --old-revision=libtasn1_2_11~20 --log=info
+#!/bin/bash
+chopem.py libtasn1-git --new-revision=libtasn1_2_11 --old-revision=libtasn1_2_11~20 --log=warning > run-example-chopit.sh
+sh run-example-chopit.sh
