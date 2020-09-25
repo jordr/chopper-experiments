24a7a37ea03540611570a39f224f6b0eeeb473bf
Jordy Ruiz <jruiz@ic.ac.uk>
2020-09-25 15:20:40 +0100

    Tweak scripts

--- auto-libtasn1/run-example.sh
+++ auto-libtasn1/run-example.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-chopem.py libtasn1-git --new-revision=libtasn1_2_11 --old-revision=libtasn1_2_11~20 --log=warning > run-example-chopit.sh
-sh run-example-chopit.sh
+chopem.py libtasn1-git --new-revision=libtasn1_2_11 --old-revision=libtasn1_2_11~20 --log=warning | tee run-example-chopits.sh
+sh run-example-chopits.sh
