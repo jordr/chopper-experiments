f1a980d8f58d5ccaa1eabf412b98f30fe89206db
Jordy Ruiz <jruiz@ic.ac.uk>
2021-01-04 17:42:54 +0000

    Update auto-bc/run-example.sh

--- auto-bc/run-example.sh
+++ auto-bc/run-example.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-chopem.py --log=info --new-revision=master bc-git
-head run-example-chopit.sh
-echo "$ chopem.py --log=info --new-revision=master bc-git"
+chopem.py --log=info --new-revision=master --execute-chopper=1 bc-git
